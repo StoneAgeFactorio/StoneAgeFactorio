db5f4cce12399f0f417ebfaa0496c3da1052fe6d
Romke van der Meulen <romke.vd.meulen@gmail.com>
2018-08-19 16:15:14 +0200

    feat: disable mining rocks without a tool

--- control/mining.lua
+++ control/mining.lua
@@ -3,33 +3,51 @@ require "stdlib/entity/entity"
 require "stdlib/string"
 
 mining_state = {
-    allowed = nil
+    allowed = {
+        resources = nil,
+        rocks = nil
+    }
 }
 
-function allow_mining(mode)
-    if mining_state.allowed == mode then return end
+function allow_resource_mining(mode)
+    if mining_state.allowed.resources == mode then return end
 
     for _, entity in ipairs(game.surfaces["nauvis"].find_entities_filtered{type="resource"}) do
         entity.minable = mode
     end
-    mining_state.allowed = mode
+    mining_state.allowed.resources = mode
+end
+
+function allow_rock_mining(mode)
+    if mining_state.allowed.rocks == mode then return end
+
+    for _, entity in ipairs(game.surfaces["nauvis"].find_entities_filtered{type="simple-entity"}) do
+        if entity.prototype.count_as_rock_for_filtered_deconstruction then
+            entity.minable = mode
+        end
+    end
+    mining_state.allowed.rocks = mode
 end
 
 Event.register(defines.events.on_player_created, function(e)
-    allow_mining(false)
+    allow_resource_mining(false)
+    allow_rock_mining(false)
 end)
 
 Event.register(defines.events.on_player_tool_inventory_changed, function(e)
     local tool_inventory = game.players[e.player_index].get_inventory(defines.inventory.player_tools)
     if (tool_inventory.is_empty()) then
-        allow_mining(false)
+        allow_resource_mining(false)
+        allow_rock_mining(false)
         return
     end
 
     local tool = tool_inventory[1]
     if tool.name == "iron-axe" then
-        allow_mining(true)
+        allow_resource_mining(true)
+        allow_rock_mining(true)
     elseif tool.name == "steel-axe" then
-        allow_mining(true)
+        allow_resource_mining(true)
+        allow_rock_mining(true)
     end
 end)
