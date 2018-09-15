7fdacc34024ef812dcdf93ea6d986040d0050915
Romke van der Meulen <romke.vd.meulen@gmail.com>
2018-09-15 18:15:24 +0200

    Implemented better random resource spawning

--- control/resources.lua
+++ control/resources.lua
@@ -12,22 +12,40 @@ local resources = {
 	"sandy-patch"
 }
 
-function spawn_resources()
-	for i, resource in ipairs(resources) do
-		-- TODO: find interesting semi-random distribution that doesn't spawn
-		-- on water or on top of other entities.
-		local x = 3 * i
-		local y = 3 * i
-		if (i % 2 == 0) then
-			x = x * -1
+local spawn_area = {x = {-40, 40}, y = {-40, 40}}
+local spawn_clearance_radius = 5
+
+function create_random_position(center, area)
+	return {
+		math.random(center.x + area.x[1], center.x + area.x[2]),
+		math.random(center.y + area.y[1], center.y + area.y[2]),
+	}
+end
+
+function find_spawn_point(surface, center)
+	for i = 1, 500, 1 do
+		local position = create_random_position(center, spawn_area)
+		local clearance_area = {
+			{position[1] - spawn_clearance_radius, position[2] - spawn_clearance_radius},
+			{position[1] + spawn_clearance_radius, position[2] + spawn_clearance_radius},
+		}
+		local entities = surface.find_entities(clearance_area)
+		local water_count = surface.count_tiles_filtered({area = clearance_area, collision_mask = "water-tile"})
+		if #entities == 0 and water_count == 0 then
+			return position
 		end
-		if (i % 3 == 0) then
-			y = y * -1
-		end
-		game.surfaces["nauvis"].create_entity {name = resource, position = {x, y}}
+	end
+	-- No suitable position found in spawn area, spawn anyway
+	return create_random_position(center, spawn_area)
+end
+
+function spawn_resources(player)
+	for _, resource in ipairs(resources) do
+		local position = find_spawn_point(player.surface, player.position)
+		player.surface.create_entity {name = resource, position = position}
 	end
 end
 
 Event.register(defines.events.on_player_created, function(e)
-	spawn_resources()
+	spawn_resources(game.players[e.player_index])
 end)
