2e2cb9692fdad05bd33281fbe5cb6066f03c8104
Romke van der Meulen <romke.vd.meulen@gmail.com>
2018-09-29 17:47:20 +0200

    fix: put messages in separate category

--- control/story.lua
+++ control/story.lua
@@ -1,5 +1,5 @@
 require "stdlib/event/event"
 
 Event.register(defines.events.on_player_created, function(e)
-	game.show_message_dialog{text = {"stoneage-msg-intro"}}
+	game.show_message_dialog{text = {"messages.stoneage-intro"}}
 end)
