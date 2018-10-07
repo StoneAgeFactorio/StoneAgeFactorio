065d1787c1d9a7ff42a5b98702ab090410bc42d8
Romke van der Meulen <romke.vd.meulen@gmail.com>
2018-10-07 18:53:40 +0200

    fix: burner-mining-drill should also take charcoal

--- prototypes/vanilla-overrides.lua
+++ prototypes/vanilla-overrides.lua
@@ -8,3 +8,6 @@ data.raw["boiler"]["boiler"].energy_source.fuel_categories = {"chemical", "charc
 
 data.raw["inserter"]["burner-inserter"].energy_source.fuel_category = nil
 data.raw["inserter"]["burner-inserter"].energy_source.fuel_categories = {"chemical", "charcoal"}
+
+data.raw["mining-drill"]["burner-mining-drill"].energy_source.fuel_category = nil
+data.raw["mining-drill"]["burner-mining-drill"].energy_source.fuel_categories = {"chemical", "charcoal"}
