c2bf0dca9cacd2253538413197740d4a667a385a
Romke van der Meulen <romke.vd.meulen@gmail.com>
2018-10-07 18:49:25 +0200

    feat: make early vanilla burners take charcoal

--- prototypes/vanilla-overrides.lua
+++ prototypes/vanilla-overrides.lua
@@ -1,1 +1,10 @@
 data.raw["recipe"]["iron-axe"].energy_required = 5
+
+data.raw["furnace"]["stone-furnace"].energy_source.fuel_category = nil
+data.raw["furnace"]["stone-furnace"].energy_source.fuel_categories = {"chemical", "charcoal"}
+
+data.raw["boiler"]["boiler"].energy_source.fuel_category = nil
+data.raw["boiler"]["boiler"].energy_source.fuel_categories = {"chemical", "charcoal"}
+
+data.raw["inserter"]["burner-inserter"].energy_source.fuel_category = nil
+data.raw["inserter"]["burner-inserter"].energy_source.fuel_categories = {"chemical", "charcoal"}
